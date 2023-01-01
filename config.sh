eed6b57d1130e48614c70c833cf543f08ffbad8e
BigFish2086 <a8686.ibrahim@gmail.com>
2023-01-01 18:57:12 +0200

    add initial setup of the project

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ./config/laserscan_multi_merger.launch ./src/ira_laser_tools/launch/laserscan_multi_merger.launch
+
+cp ./config/summit_xl_one_robot.launch ./src/summit_xl_sim/summit_xl_gazebo/launch/summit_xl_one_robot.launch
---
+++ ira_tools_up.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+roslaunch ira_laser_tools laserscan_multi_merger.launch
